3f723fa9b4f8dfb33efdf4edc72bc26f9dbf0998
Hasssaanali <syedhasssaaan@gmail.com>
2023-05-02 19:20:29 +0500

    style fixes

--- src/Components/Homepage.jsx
+++ src/Components/Homepage.jsx
@@ -1,19 +1,28 @@
 import React, { Fragment } from 'react'
 import Fade from "react-reveal/Fade";
+import data from "../Data";
+import ReadMoreReact from "read-more-react";
 
 export default function Homepage() {
   return (
+   
     <Fragment>
         <Fade left>
+        
         <div className='container'>
             <div className='row'>
+            {data.Homep.map((item, index) => (
                 <div className='col-sm-12 homepagesec'>
-                    <h2>Secure Your Future and Protect Your Loved Ones with Our Innovative Digital Takaful Solutions!</h2>
-                    <p>Salaam Family Takaful Limited is a revolutionary force in the world of takaful, leading the way as Pakistan's premier digital takaful company. Utilizing state-of-the-art technology, SFTL offers its customers a seamless and easy-to-use online experience. With a mission to bring takaful to the masses in Pakistan, SFTL offers an array of diverse takaful products tailored to meet the unique needs of each individual customer. Dedicated to providing unparalleled customer service and delivering products that offer exceptional value, Salaam Family Takaful Limited is truly the premier choice for all your takaful needs.</p>
+                    <h2>{item.title}</h2>
+                    {/* <p></p> */}
+                    <ReadMoreReact text={item.desc} min={180} ideal={200}  max={270}  readMoreText="read more" />
                 </div>
+              ))}
             </div>
         </div>
+        
         </Fade>
     </Fragment>
+   
   )
 }
--- src/Data.js
+++ src/Data.js
@@ -1,4 +1,11 @@
 const data = {
+
+    Homep:[
+        {
+            "title":"Secure Your Future and Protect Your Loved Ones with Our Innovative Digital Takaful Solutions!",
+            "desc":"Salaam Family Takaful Limited is a revolutionary force in the world of takaful, leading the way as Pakistan's premier digital takaful company. Utilizing state-of-the-art technology, SFTL offers its customers a seamless and easy-to-use online experience. With a mission to bring takaful to the masses in Pakistan, SFTL offers an array of diverse takaful products tailored to meet the unique needs of each individual customer. Dedicated to providing unparalleled customer service and delivering products that offer exceptional value, Salaam Family Takaful Limited is truly the premier choice for all your takaful needs.",
+        },
+    ],
     About: [
         {
             "title": "Revolutionizing Takaful with Cutting-Edge Technology – Learn About Us)",
